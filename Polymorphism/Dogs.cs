7f3ea82b441207e0663ccb679cb4e1a09811f742
Hanisha Pagarani <hanishap@usf.edu>
2021-03-21 21:04:43 -0400

    Add project files.

---
+++ Polymorphism/Dogs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms; // Needed for MessageBox
+
+namespace Polymorphism
+{
+    class Dogs : Animal
+    {
+        // Field
+        private string _name;
+
+        // Constructor
+        public Dogs(string name)
+            : base("Dog")
+        {
+            _name = name;
+        }
+
+        // Name property
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // MakeSound method
+        public override void MakeSound()
+        {
+            MessageBox.Show("Woof! Woof!");
+        }
+    }
+}
